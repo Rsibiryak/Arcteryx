36c600cbd28a965fa3a4169192616b648b6a5f28
Alexander S <AlexSuvv@gmail.com>
2019-05-07 17:27:30 +0300

    Commit all files.

---
+++ Arcteryx/Arcteryx/Pages/MainPage.cs
@@ -0,0 +1,20 @@
+﻿using Arcteryx.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcteryx.Pages
+{
+    class MainPage : PageBase
+    {
+        #region
+        #endregion
+
+        public MainPage(AppManager appManager) : base(appManager)
+        {
+        }
+
+    }
+}
