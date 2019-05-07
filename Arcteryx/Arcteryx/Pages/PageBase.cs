6dc61300efe84d306c89cea39dcec56586b62ce1
Alexander_Suvorov <Alexander_Suvorov@epam.com>
2019-05-07 15:33:35 +0300

    The first version.

---
+++ Arcteryx/Arcteryx/Pages/PageBase.cs
@@ -0,0 +1,29 @@
+﻿using Arcteryx.Utils;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcteryx.Pages
+{
+    public abstract class  PageBase
+    {
+        public IWebDriver Driver {get;}
+        public PageBase(AppManager appManager) {
+            this.Driver = appManager.Driver;
+        }
+
+        /// <summary>
+        /// Return dom tree from shadow-root 
+        /// </summary>
+        /// <param name="domTree"></param>
+        /// <returns></returns>
+        public IWebElement expandRootElement(IWebElement domTree)
+        {
+            IWebElement element = (IWebElement)((IJavaScriptExecutor)Driver).ExecuteScript("return arguments[0].shadowRoot", domTree);
+            return element;
+        }
+    }
+}
---
+++ Arcteryx/Arcteryx/Utils/AppManager.cs
@@ -0,0 +1,61 @@
+﻿using Arcteryx.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcteryx.Utils
+{
+    public class AppManager
+    {
+        public static AppManager Instance { get; private set; }
+        public IWebDriver Driver { get; private set; }
+        public MainPage MainPg { get; private set; }
+
+
+        private AppManager()
+        {
+            Driver = new ChromeDriver();
+            MainPg = new MainPage(this);
+        }
+
+
+        public AppManager GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new AppManager();
+            }
+            return this;
+        }
+
+    }
+}
+
+
+
+
+
+public IWebDriver Driver { get; private set; }
+public LoginPage LoginPg { get; private set; }
+public DashboardPage DashboardPg { get; private set; }
+
+
+private ApplicationManager()
+{
+    Driver = new ChromeDriver();
+    LoginPg = new LoginPage(this);
+    DashboardPg = new DashboardPage(this);
+}
+
+public static ApplicationManager GetInstance()
+{
+    if (Instance == null)
+    {
+        Instance = new ApplicationManager();
+    }
+    return Instance;
+}
