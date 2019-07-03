0d75dc783dc22acfc962a0c5d35437ac16c12ede
Alexander S <AlexSuvv@gmail.com>
2019-07-03 16:55:44 +0300

    Fix log4net error.

---
+++ Arcteryx/Arcteryx/CopyFiles.bat
@@ -0,0 +1,5 @@
+cd..
+copy   Arcteryx\ReportPortal.addins packages\NUnit.ConsoleRunner.3.10.0\tools\
+mkdir  packages\NUnit.ConsoleRunner.3.10.0\tools\lib
+copy   Arcteryx\bin\Debug\ReportPortal.NUnitExtension.dll  packages\NUnit.ConsoleRunner.3.10.0\tools\lib\
+pause
